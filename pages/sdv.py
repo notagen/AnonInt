8b5d0d628877a656c3db3b75398df841de2da536
notagen <harristsokas@gmail.com>
2024-02-26 00:33:18 +0200

    generate synthesizers pages changes

--- pages/sdv.py
+++ pages/sdv.py
@@ -4,10 +4,11 @@ import streamlit as st
 from sdv.metadata import SingleTableMetadata
 from sdv.single_table import CTGANSynthesizer
 from sdv.single_table import GaussianCopulaSynthesizer
+from sdv.evaluation.single_table import evaluate_quality
+from sdv.evaluation.single_table import get_column_plot
 
 
 real = st.session_state.template
-
 st.write(real)
 metadataButton = st.button('Print Metadata')
 if metadataButton:
@@ -17,16 +18,48 @@ if metadataButton:
     metadata.validate()
     st.write(metadata)
 
+
+
 gaussianButton = st.button('Generate Synthetic Gaussian')
 if gaussianButton:
     gaussianSynth = GaussianCopulaSynthesizer(st.session_state.metadata)
     gaussianSynth.fit(real)
-    synthetic_data_gaussian = ctganSynth.sample(100)
+    synthetic_data_gaussian = gaussianSynth.sample(1000)
     st.write(synthetic_data_gaussian)
+    quality_report_gaussian = evaluate_quality(
+    real_data=real,
+    synthetic_data=synthetic_data_gaussian,
+    metadata=st.session_state.metadata)
+    st.write(quality_report_gaussian.get_visualization(property_name='Column Shapes'))
+    st.write(quality_report_gaussian.get_visualization(property_name='Column Pair Trends'))
+    for i in real.columns.tolist():
+        fig = get_column_plot(
+            real_data=real,
+            synthetic_data=synthetic_data_gaussian,
+            column_name=i,
+            metadata=st.session_state.metadata
+        )
+        #fig.show()
+        st.write(fig)
 
 ctganButton = st.button('Generate Synthetic CTGAN')
 if ctganButton:
     ctganSynth = CTGANSynthesizer(st.session_state.metadata)
-    ctganSynth.fit(real)
-    synthetic_data_ctgan = ctganSynth.sample(100)
+    ctganSynth.fit(real.sample(300))
+    synthetic_data_ctgan = ctganSynth.sample(1000)
     st.write(synthetic_data_ctgan)
+    quality_report_ctgan = evaluate_quality(
+    real_data=real,
+    synthetic_data=synthetic_data_ctgan,
+    metadata=st.session_state.metadata)
+    st.write(quality_report_ctgan.get_visualization(property_name='Column Shapes'))
+    st.write(quality_report_ctgan.get_visualization(property_name='Column Pair Trends'))
+    for i in real.columns.tolist():
+        fig = get_column_plot(
+            real_data=real,
+            synthetic_data=synthetic_data_ctgan,
+            column_name=i,
+            metadata=st.session_state.metadata
+        )
+        #fig.show()
+        st.write(fig)
